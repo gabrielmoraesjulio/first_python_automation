2c18bb5fd08ae71ef849310ef9e8f9edac84f813
Gabriel <golk76@outlook.com>
2025-04-09 11:30:40 -0300

    First commit

---
+++ invoices.py
@@ -0,0 +1,10 @@
+import os
+from openpyxl import Workbook
+
+directory = 'pdf_invoices' #invoices directory
+files = os.listdir(directory) #what are the invoices
+files_quantity = len(files) #quantity of invoices
+
+if files_quantity == 0: #if the folder are empty
+    raise Exception("No files found in the directory") #raise a exception to warn the user
+
